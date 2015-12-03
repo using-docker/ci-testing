5e15eea2cef38df6239397bde34acf7ca949a03a
Adrian Mouat <adrian@adrianmouat.com>
2015-12-04 09:06:51 +1100

    Updated cmd.sh to run tests.

--- identidock/cmd.sh
+++ identidock/cmd.sh
@@ -4,8 +4,11 @@ set -e
 if [ "$ENV" = 'DEV' ]; then
   echo "Running Development Server"
   exec python "identidock.py"
+elif [ "$ENV" = 'UNIT' ]; then 
+  echo "Running Unit Tests"
+  exec python "tests.py"
 else
   echo "Running Production Server"
   exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
-             --callable app --stats 0.0.0.0:9191
+                 --callable app --stats 0.0.0.0:9191
 fi
